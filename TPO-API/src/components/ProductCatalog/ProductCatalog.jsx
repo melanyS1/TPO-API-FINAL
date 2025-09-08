d5dd2e9983c47a8e338b1441402437394772a10a
lulimiraanda <lam10500@gmail.com>
2025-09-08 14:04:17 -0300

    Funcionalidad para ver detalle del catalogo

--- TPO-API/src/components/ProductCatalog/ProductCatalog.jsx
+++ TPO-API/src/components/ProductCatalog/ProductCatalog.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import ProductCard from '../Products/ProductCard';
-import products from '../../services/products.js';
+import useGetProducts from '../../hooks/useGetProducts';
 import './ProductCatalog.css';
 
-
 const ProductCatalog = () => {
+    const products = useGetProducts();
     return (
         <div className="product-catalog">
             <div className="products-grid">
-                {products.map(product => (
-                    <ProductCard key={product.id} product={product} />
-                ))}
+                {products && products.length > 0 ? (
+                    products.map(product => (
+                        <ProductCard key={product.id} product={product} />
+                    ))
+                ) : (
+                    <div>Cargando productos...</div>
+                )}
             </div>
         </div>
     );
--- TPO-API/src/components/Products/ProductCard.jsx
+++ TPO-API/src/components/Products/ProductCard.jsx
@@ -1,15 +1,26 @@
+
 import React from 'react';
 import './ProductCard.css';
+import { useNavigate } from 'react-router-dom';
 
 const ProductCard = ({ product }) => {
+    const navigate = useNavigate();
     return (
-        <div className="product-card">
+        <div
+            className="product-card"
+            style={{ cursor: 'pointer' }}
+            onClick={() => window.open(`/products/${product.id}`, '_blank')}
+        >
             <img src={product.image} alt={product.name} className="product-image" />
             <div className="product-info">
                 <h3 className="product-name">{product.name}</h3>
                 <p className="product-description">{product.description}</p>
                 <p className="product-price">${product.price}</p>
-                <button className="add-to-cart">Add to Cart</button>
+                <button className="add-to-cart"
+                    onClick={e => {
+                        e.stopPropagation();
+                    }}
+                >Add to Cart</button>
             </div>
         </div>
     );
