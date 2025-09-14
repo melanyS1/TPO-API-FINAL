120ca8b34adcd4c284264805b338a32653444747
louravalos <128548894+louravalos@users.noreply.github.com>
2025-09-14 19:04:47 -0300

    Pongo como estaba antes ProductCard.jsx

--- TPO-API/src/components/Products/ProductCard.jsx
+++ TPO-API/src/components/Products/ProductCard.jsx
@@ -15,7 +15,7 @@ const ProductCard = ({ product }) => {
             <div className="product-info">
                 <h3 className="product-name">{product.name}</h3>
                 <p className="product-description">{product.description}</p>
-                <p className="product-price">${Number(product.price).toFixed(2)}</p>
+                <p className="product-price">${product.price}</p>
                 <button 
                     className="add-to-cart"
                     onClick={e => {
