f7d403912bb8ce5600d087369bd8603795dc9ae7
RoxRodriguez <rrodriguez@uade.edu.ar>
2025-09-15 01:51:14 -0300

    Mapeo de ruta protegida en Browse Router

--- TPO-API/src/App.jsx
+++ TPO-API/src/App.jsx
@@ -10,6 +10,7 @@ import ProductDetail from "./pages/ProductDetail";
 import ThankYouPage from "./pages/ThankYouPage/ThankYouPage";
 import Products from "./pages/Products/Products";
 import MisProductos from "./pages/MisProductos/misProductos";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
@@ -24,11 +25,16 @@ function App() {
               <Route path="/login" element={<LoginUser />} />
               <Route path="/register" element={<RegisterUser />} />
               <Route path="/products" element={<Products />} />
-              <Route path="/products/category/:categoryId" element={<Products />} />
-              <Route path="/cart" element={<Cart />} />
+              <Route path="/products/category/:categoryId" element={<Products />} /> 
               <Route path="/products/:id" element={<ProductDetail />} />
               <Route path="/thank-you" element={<ThankYouPage />} />
               <Route path="/mis-productos" element={<MisProductos />} />
+              <Route path="/cart" 
+                element={
+                  <ProtectedRoute>
+                    <Cart />
+                  </ProtectedRoute>
+                } />
             </Routes>
           </main>
         </Router>
