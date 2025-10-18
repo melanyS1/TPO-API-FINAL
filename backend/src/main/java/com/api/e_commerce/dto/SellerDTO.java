8d54f40d8d68ec58ac5eadd317fe5717f40ac62e
Edith062 <edith.mamani062@gmail.com>
2025-10-18 16:45:19 -0300

    Add DTO and model classes for Product and Seller, including Category association

---
+++ backend/src/main/java/com/api/e_commerce/dto/SellerDTO.java
@@ -0,0 +1,11 @@
+package com.api.e_commerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SellerDTO {
+    private Long id;
+    private String username;
+}
